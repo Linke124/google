5110e9e1242cc7357a0349c264dd156ffa8f02cd
Linke124 <115611816+Linke124@users.noreply.github.com>
2022-10-12 15:10:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function search(){
+    let x = document.getElementById('search').value;
+    window.open('https://www.google.com/search?q='+x);
+}
+
+function imlucky(){
+    window.open('https://www.google.com/doodles');
+}
+
